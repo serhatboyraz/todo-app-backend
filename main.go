e1001901b4a0c342ed7850c3f8670e0ea4984534
Serhat Boyraz <stbyrz@outlook.com>
2021-04-16 12:49:59 +0300

    delete todo request

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
@@ -13,17 +15,20 @@ import (
 
 type (
 	Todo struct {
-		Title  string `bson:"title" json:"title" validate:"required"`
-		Priority string `bson:"priority" json:"priority" validate:"required"`
+		Id       primitive.ObjectID `bson:"_id" json:"id"`
+		Title    string             `bson:"title" json:"title" validate:"required"`
+		Priority string             `bson:"priority" json:"priority" validate:"required"`
 	}
 )
 
 func main() {
 	// Echo instance
 	e := echo.New()
+	e.Use(middleware.CORS())
 
 	e.GET("/todo", getToDoList)
 	e.PUT("/todo", addToDo)
+	e.DELETE("/todo", deleteToDo)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
@@ -51,8 +56,31 @@ func addToDo(c echo.Context) (err error) {
 	var client, _ = getMongoConnection(ctx)
 
 	collection := client.Database("todoApp").Collection("todos")
+	todo.Id = primitive.NewObjectID()
 	collection.InsertOne(ctx, todo)
 	return c.JSON(http.StatusOK, todo)
+}
+
+func deleteToDo(c echo.Context) (err error) {
+	todoId := c.QueryParam("todoId")
+	idPrimitive, err := primitive.ObjectIDFromHex(todoId)
+
+	var ctx, _ = getContext()
+	var client, _ = getMongoConnection(ctx)
+
+	collection := client.Database("todoApp").Collection("todos")
+
+	var todo Todo
+	if err = collection.FindOne(ctx, bson.M{"_id": idPrimitive}).Decode(&todo); err != nil {
+		return c.NoContent(http.StatusNotFound)
+	}
+	deleteResult, _ := collection.DeleteOne(ctx, bson.M{"_id": idPrimitive})
+
+	if deleteResult.DeletedCount > 0 {
+		return c.NoContent(http.StatusOK)
+	} else {
+		return c.NoContent(http.StatusInternalServerError)
+	}
 }
 
 func getContext() (context.Context, context.CancelFunc) {
